c168fc86f69153e7b6b0acb60da34452d8d1b0ef
Joseph Wilfong <josephwilfong91@gmail.com>
2023-05-26 15:15:17 -0700

    add some more HTML, CSS, and edit JavaScript

--- Factory/wwwroot/js/main.js
+++ Factory/wwwroot/js/main.js
@@ -1,4 +1,16 @@
 document.getElementById('menu-button').addEventListener('click', function () {
   var menuIcon = document.getElementById('menu-icon');
-  menuIcon.classList.toggle('flip');
+  
+  if (menuIcon.classList.contains('flip')) {
+    menuIcon.classList.remove('flip');
+  } else {
+    menuIcon.classList.add('flip');
+  }
 });
+
+
+
+
+
+
+
