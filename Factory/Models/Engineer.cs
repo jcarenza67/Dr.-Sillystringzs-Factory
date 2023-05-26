11750fb0c9f0ec082d309a3992edb1f257628e5e
Joseph Wilfong <josephwilfong91@gmail.com>
2023-05-26 09:44:11 -0700

    add contents for Engineer controller, Engineer.cs, and EngineerMachine.cs

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,9 +1,19 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Factory.Models
 {
-  public class ClassName
+  public class Engineer
   {
-    public string PropertyName { get; set; }
+    public int EngineerId {get; set;}
+    [Required(ErrorMessage = "Engineer First name can't be left empty")]
+    public string EngineerFName {get; set;}
+    [Required(ErrorMessage = "Engineer Last name can't be left empty")]
+    public string EngineerLName {get; set;}
+    [Required(ErrorMessage = "Engineer License can't be left empty")]
+    public string EngineerLicense {get; set;}
+    public int MachineId { get; set; }
+    public List<EngineerMachine> JoinEntities {get;}
   }
 }
