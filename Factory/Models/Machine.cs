3f4cc9489f2dc0c9af3b8c671eb3d97012fef86b
Joseph Wilfong <josephwilfong91@gmail.com>
2023-05-26 14:36:05 -0700

    fix some buttons, add some javascript and more styling. WIP still styling

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -10,8 +10,11 @@ namespace Factory.Models
     public int MachineId { get; set; }
     [Required(ErrorMessage = "Machine type can't be left empty")]
     public string MachineType { get; set; }
+    [Required(ErrorMessage = "Machine service can't be left empty")]
     public string LastServiced {get; set;}
+    [Required(ErrorMessage = "Machine age can't be left empty")]
     public string Age {get; set;}
+    [Required(ErrorMessage = "Machine description can't be left empty")]
     public string MachineDescription { get; set; }
     public List<EngineerMachine> JoinEntities {get;} 
   }
---
+++ Factory/wwwroot/js/main.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-button').addEventListener('click', function () {
+  var menuIcon = document.getElementById('menu-icon');
+  menuIcon.classList.toggle('flip');
+});
