b8c53630db5aaee8eeb940ee34bfadd33c938f75
Joseph Wilfong <josephwilfong91@gmail.com>
2023-05-26 10:48:49 -0700

    fix versions, add packages, create migrations and a database

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Factory.Models;
 
 namespace Factory
 {
@@ -10,6 +12,14 @@ namespace Factory
       WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
       builder.Services.AddControllersWithViews();
+
+      builder.Services.AddDbContext<FactoryContext>(
+                        dbContextOptions => dbContextOptions
+                          .UseMySql(
+                            builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                          )
+                        )
+                      );
 
       WebApplication app = builder.Build();
 
